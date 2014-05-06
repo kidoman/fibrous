89702760f9786e49849b6a543f4f0f3a366c2169
Karan Misra <kidoman@gmail.com>
2014-05-06 19:51:31 +0530

    python: extract User module

--- python/twisted_server.py
+++ python/twisted_server.py
@@ -2,40 +2,7 @@ from twisted.web.resource import Resource
 from twisted.web.server import Site
 from twisted.internet import reactor
 
-import json
-import redis
-
-redis_connection = redis.StrictRedis(host='localhost', port=6379, db=0)
-
-class User:
-    def __init__(self, id, name):
-        self._id = id
-        self._name = name
-
-    @classmethod
-    def fromRequest(cls, content):
-        user_map = json.loads(content.getvalue())
-        return User(user_map["id"], user_map["name"])
-
-    @classmethod
-    def load(cls, id):
-        name = redis_connection.get(User._key(id))
-        if name is None:
-            return None
-        return User(id, name)
-
-    @classmethod
-    def _key(cls, id):
-        return "user:" + str(id)
-
-    def name(self):
-        return self._name
-
-    def save(self):
-        redis_connection.set(User._key(self._id), self._name)
-
-    def to_json(self):
-        return json.dumps({"id": self._id, "name": self._name})
+from user import User
 
 class UserResource(Resource):
     isLeaf = True
---
+++ python/user.py
@@ -0,0 +1,34 @@
+import json
+import redis
+
+redis_connection = redis.StrictRedis(host='localhost', port=6379, db=0)
+
+class User:
+    def __init__(self, id, name):
+        self._id = id
+        self._name = name
+
+    @classmethod
+    def fromRequest(cls, content):
+        user_map = json.loads(content.getvalue())
+        return User(user_map["id"], user_map["name"])
+
+    @classmethod
+    def load(cls, id):
+        name = redis_connection.get(User._key(id))
+        if name is None:
+            return None
+        return User(id, name)
+
+    @classmethod
+    def _key(cls, id):
+        return "user:" + str(id)
+
+    def name(self):
+        return self._name
+
+    def save(self):
+        redis_connection.set(User._key(self._id), self._name)
+
+    def to_json(self):
+        return json.dumps({"id": self._id, "name": self._name})
