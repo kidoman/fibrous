c77ac47c32d78a43068d8931f91b8e4d34ce5fb4
Karan Misra <kidoman@gmail.com>
2014-05-10 08:28:42 +0530

    go: allow the redis capacity to be configurable

--- go/redis.go
+++ go/redis.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"os"
+	"strconv"
 	"time"
 
 	"github.com/garyburd/redigo/redis"
@@ -15,10 +17,28 @@ func (r *resourceConn) Close() {
 	r.Conn.Close()
 }
 
+func redisConnParams() int {
+	capStr := os.Getenv("REDIS_CAP")
+	if capStr == "" {
+		capStr = "20"
+	}
+	cap, err := strconv.Atoi(capStr)
+	if err != nil {
+		panic(err)
+	}
+
+	return cap
+}
+
+const redisMaxCap = 200
+
 func newPool(server string) *pools.ResourcePool {
 	f := func() (pools.Resource, error) {
 		c, err := redis.Dial("tcp", server)
 		return &resourceConn{c}, err
 	}
-	return pools.NewResourcePool(f, 3, 20, time.Minute)
+
+	cap := redisConnParams()
+
+	return pools.NewResourcePool(f, cap, redisMaxCap, time.Minute)
 }
