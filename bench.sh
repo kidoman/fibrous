b2791f6521cbc5454011c175dfd5c3e14ec141ae
Karan Misra <kidoman@gmail.com>
2014-05-10 08:28:23 +0530

    allow more control over benching

--- bench.sh
+++ bench.sh
@@ -5,4 +5,4 @@ go get github.com/rakyll/boom
 
 curl -XPOST -H "Content-Type: application/json" -d '{"id": 1, "name": "Rob Pike"}' http://127.0.0.1:3000/users
 
-boom -n 100000 -c 100 http://127.0.0.1:3000/users/1
+boom -n ${T:-100000} -c ${C:-200} http://127.0.0.1:3000/users/1
