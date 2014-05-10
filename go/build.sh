2e84c17be3df8dbaa2f60185ad3224d9ee952445
Karan Misra <kidoman@gmail.com>
2014-05-10 07:15:18 +0530

    go: get more packages

--- go/build.sh
+++ go/build.sh
@@ -3,6 +3,9 @@ set -e
 
 go get github.com/garyburd/redigo/redis
 go get github.com/youtube/vitess/go/pools
+go get github.com/go-martini/martini
+go get github.com/martini-contrib/binding
+go get github.com/martini-contrib/encoder
 
 go build -o stdlib db.go redis.go user.go apiserver.go stdlib.go
 go build -o martini db.go redis.go user.go martini.go
