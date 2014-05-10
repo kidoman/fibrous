23631395c5ea2d4cfae5070a09c343e2724d35d7
Karan Misra <kidoman@gmail.com>
2014-05-10 07:13:31 +0530

    go: get required packages

--- go/build.sh
+++ go/build.sh
@@ -1,5 +1,8 @@
 #!/bin/bash --login
 set -e
 
+go get github.com/garyburd/redigo/redis
+go get github.com/youtube/vitess/go/pools
+
 go build -o stdlib db.go redis.go user.go apiserver.go stdlib.go
 go build -o martini db.go redis.go user.go martini.go
