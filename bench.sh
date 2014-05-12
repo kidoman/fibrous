ce7bdfa9621876e1d855ea1207ff82624ebfad13
Karan Misra <kidoman@gmail.com>
2014-05-12 13:39:35 +0530

    adjust bench parameters

--- bench.sh
+++ bench.sh
@@ -5,4 +5,4 @@ go get github.com/rakyll/boom
 
 curl -XPOST -H "Content-Type: application/json" -d '{"id": 1, "name": "Rob Pike"}' http://127.0.0.1:3000/users
 
-boom -n ${T:-100000} -c ${C:-200} http://127.0.0.1:3000/users/1
+GOMAXPROCS=4 boom -n ${T:-100000} -c ${C:-50} http://127.0.0.1:3000/users/1
