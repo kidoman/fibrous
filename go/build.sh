b0c14213b8f973a745343e9b2c88e1a4466ef1cd
Faris Zacina <faris.zacina@gmail.com>
2016-04-24 13:35:07 +0200

    Added gocraft into the mix

--- go/build.sh
+++ go/build.sh
@@ -7,7 +7,9 @@ go get github.com/go-martini/martini
 go get github.com/martini-contrib/binding
 go get github.com/martini-contrib/encoder
 go get github.com/gorilla/mux
+go get github.com/gocraft/web
 
 go build -o stdlib db.go redis.go user.go apiserver.go stdlib.go
 go build -o gorilla db.go redis.go user.go gorilla.go
 go build -o martini db.go redis.go user.go martini.go
+go build -o gocraft db.go redis.go user.go gocraft.go
